b70fadd295d34805ae59ace599f123a5d0d7975f
1stfaccia1 <chad.m.wittman75@gmail.com>
2018-11-12 16:55:33 -0600

    added

--- schema.js
+++ schema.js
@@ -11,7 +11,7 @@ const LaunchType = new GraphQLObjectType({
     launch_year: { type: GraphQLString},
     launch_date_local: { type: GraphQLString},
     launch_success: { type: GraphQLBoolean},
-    launch_year: { type: RocketType},
+    rocket: { type: RocketType},
 
 
   })
@@ -23,9 +23,6 @@ const RocketType = new GraphQLObjectType({
     rocket_id: {type: GraphQLString},
     rocket_name: {type: GraphQLString},
     rocket_type: { type: GraphQLString},
-
-
-
   })
 });
 
@@ -38,7 +35,33 @@ const RootQuery = new GraphQLObjectType({
         return Axios.get('https://api.spacexdata.com/v3/launches')
         .then(res => res.data);
       }
-
+    },
+    launch: {
+      type: LaunchType,
+      args:{
+        flight_number: { type: GraphQLInt }
+      },
+      resolve(parent, args) {
+        return Axios.get(`https://api.spacexdata.com/v3/launches/${args.flight_number}`)
+        .then(res => res.data);
+      }
+    },
+    rockets: {
+      type: new GraphQLList(RocketType),
+      resolve(parent, args){
+        return Axios.get('https://api.spacexdata.com/v3/rockets')
+        .then(res => res.data);
+      }
+    },
+    rocket: {
+      type: RocketType,
+      args:{
+        id: { type: GraphQLInt }
+      },
+      resolve(parent, args) {
+        return Axios.get(`https://api.spacexdata.com/v3/rockets/${args.id}`)
+        .then(res => res.data);
+      }
     }
   }
 });
