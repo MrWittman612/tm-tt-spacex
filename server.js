4c0018652a70f3e14b41ed32da63511cbfdb1b7a
1stfaccia1 <chad.m.wittman75@gmail.com>
2018-11-14 23:35:45 -0600

    idk

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const cors = require('cors');
 const schema = require('./schema');
 const app = express();
+
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
   schema,
