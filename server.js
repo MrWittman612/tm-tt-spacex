7e1cd72a4a1599cf27bb5493cdd3d7b0e44cff6f
1sfaccia1 <chad.m.wittman75@gmail.com>
2019-05-09 03:54:21 -0500

    app not working switch path

--- server.js
+++ server.js
@@ -18,9 +18,16 @@ app.use(
 
 app.use(express.static('public'));
 
-app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-});
+// app.get('*', (req, res) => {
+// 	res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+// });
+if (process.env.NODE_ENV === 'production') {
+	app.use('/static', express.static(path.join(__dirname, 'client/build')));
+	// app.use( express.static( 'client/build' ) );
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 
