2e4e9c29d6ba7830499af3a5243a4ae51b065c13
1sfaccia1 <chad.m.wittman75@gmail.com>
2019-05-09 04:15:23 -0500

    app not working switch path

--- server.js
+++ server.js
@@ -22,13 +22,13 @@ app.use(express.static('public'));
 // 	res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 // });
 if (process.env.NODE_ENV === 'production') {
-	app.use('/static', express.static(path.join(__dirname, 'client/build')));
-	// app.use( express.static( 'client/build' ) );
+	// app.use('/static', express.static(path.join(__dirname, 'client/build')));
+	app.use(express.static('client/build'));
 	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
 }
 
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+app.listen(port, () => console.log(`Server started on ${port}`));
