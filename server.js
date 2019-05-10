338fe7d3117096397daaea89ec5fe54f8625b388
1sfaccia1 <chad.m.wittman75@gmail.com>
2019-05-09 22:48:27 -0500

    not working on heroku

--- server.js
+++ server.js
@@ -14,13 +14,13 @@ app.use(
 	})
 );
 
-// if (process.env.NODE_ENV === 'production') {
-// 	app.use('/static', express.static(path.join(__dirname, 'client/build')));
-// 	// app.use( express.static( 'client/build' ) );
-// 	app.get('*', (req, res) => {
-// 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-// 	});
-// }
+if (process.env.NODE_ENV === 'production') {
+	// app.use('/static', express.static(path.join(__dirname, 'client/build')));
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const port = process.env.PORT || 5000;
 
